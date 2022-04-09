138947eb880529fb38a0c1a75a8d8d4953e0cc43
Moritz Hausmann <103260415+mrtzhsmnn@users.noreply.github.com>
2022-04-09 20:40:15 +0200

    Aufg. 5 Fertig, Hilfsfunktionen hinzugefügt.

--- affchiff.py
+++ affchiff.py
@@ -77,7 +77,7 @@ key_table = {
 #       Erwartet String plain_text mit dem zu verschlüsselnden Text.
 #   Verschlüsselt plain_text mit dem Schlüsselpaar a & b.
 #   Fehler, falls mind. einer der Schlüssel ungültig. => Fehlermeldung wird ausgegeben, leerer String zurückgegeben.
-#   ==> Gibt String mit verschlüsseltem Text aus, bei Fehler leeren String.
+#   ==> Gibt String mit verschlüsseltem Text zurück (nur Großbuchstaben), bei Fehler leeren String.
 def acEncrypt(a, b , plain_text):
     #String für return
     retstring = ''
@@ -103,6 +103,79 @@ def acEncrypt(a, b , plain_text):
     return retstring.swapcase()
    
 
+#Funktion "acDecrypt":
+#   IN: Erwartet Schlüssel a, und b, beide müssen in key_table sein.
+#       Erwartet String cypher_text mit dem zu entschlüsselnden Text.
+#   Entschlüsselt cypher_text mit dem Schlüsselpaar a & b.
+#   Fehler, falls mind. einer der Schlüssel ungültig. => Fehlermeldung wird ausgegeben, leerer String zurückgegeben.
+#   ==> Gibt String mit entschlüsseltem Text zurück (nur Kleinbuchstaben), bei Fehler leeren String.
+def acDecrypt(a, b, cypher_text):
+    #Liste um Zahlen zur Manipulation zu speichern. i für counter bei for-Schleife
+    worklist = []
+    i = 0
+    #Prüfen, ob beide Keys in key_table liegen (nur dann hat man gültiges Schlüsselpaar).
+    if not((a in key_table) and (b in key_table)):
+        #Wenn einer der beiden Keys ungültig, Fehlermeldung ausgeben und leeren String zurückgeben.
+        print('FEHLER: mindestens einer der Keys im KEYPAIR ist ungültig!')
+        return ''
+    #cypher_text in Liste von Zahlen umwandeln mit decode.
+    worklist = decode(cypher_text)
+    #Worklist durchlaufen, wert immer in x speichern.
+    for x in worklist:
+        #Worklist an i-ter Stelle mit entschl. Text füllen. Formel siehe Aufgabenstellung Inverses zu a ist in key_table
+        worklist[i] = (((x-b) * key_table[a])%26)
+        #i um 1 erhöhen.
+        i+=1 
+    #Worklist mit Encode zu string umwandeln und zurückgeben.
+    return encode(worklist)
+
+
+
+#HILFSFunktion "keyHelp":
+#   IN: Erwartet String mit Schlüsselpaar z.b. 'db'.
+#   Prüft und decoded Schlüsselpaar:
+#   ==> Gibt zwei Zahlen a,b als Int zurück, die als Schlüssel weiter verwendet werden können. Beide Zahlen 27 wenn Fehler.
+def keyHelp(key):
+    #Sicherstellen, dass String tatsächlich nur 2 char enthält.
+    if len(key) == 2 :
+        #String decoden und in Worklist speichern.
+        worklist = decode(key)
+        #prüfen, ob worklist auch noch 2 Elemente hat, oder ob decode Zeichen aussortiert hat
+        if len(worklist) == 2 :
+            #einzelne Keys aus Worklist
+            a = worklist[0]
+            b = worklist[1]
+            if not((a in key_table) and (b in key_table)):
+                #Wenn einer der beiden Keys ungültig, Fehlermeldung ausgeben und leeren String zurückgeben.
+                print('FEHLER: mindestens einer der Keys im KEYPAIR ist ungültig!')
+                return 27,27
+            else: return a,b #Gebe korrektes Keypair zurück
+        else:
+            print('Im vorliegenden String waren verbotene Zeichen, Sodass er beim Aufruf von "decode" verkürzt wurde!')
+            return 27,27
+    else: 
+        print('Der String ist entweder <2 oder >2 Zeichen. Ein keypair besteht aus 2 Zeichen.')
+        return 27,27
+
+
+#------V E R M U T L I C H   O B S O L E T------
+#HILFSFunktion "exGCD"(
+#   IN: Erwartet a und b mit denene erw. Euklid. berechnet werden sollen.
+#   Berechnet rekursiv den erweiterten Euklidischen Algorithmus
+#   ==> Gibt als erste Variable die eigentliche Lösung, als zweite "x" und als letzte "y" aus. y mod Z ergibt dann das Inverse zu a 
+def exGCD(x, y):
+    #Abbruchbedingung:
+    if x == 0 :
+        #Rückgabe von dem Ergebnis, sowie von dem Inversen. 
+        return y,0,1
+    #Rekursion:
+    gcd,x1,y1 = exGCD(y%a, a)
+    #Update x und y mit den Resultaten aus der Rekursion:
+    x = y1 - (y//x) * x1
+    y = x1
+    return gcd,x,y
+
+
 #--TESTS:--
 #Test von decode wie in Aufgabenstellung:
 print('Test von "decode" mit: ')
@@ -122,9 +195,14 @@ print('Test von "encode" mit: ')
 print(char_list)
 print('Ergebnis: ' + encode(char_list))
 print('-----------------------------------------')
-#Test von acencrypt mit aufgabenstellung
+#Test von acEncrypt mit aufgabenstellung
 teststring = 'botschaft'
 print('Testen von acEncrypt mit : ' + teststring)
 a = 11 
 b = 23
 print('Ergebnis: ' + acEncrypt(a,b,teststring))
+print('-----------------------------------------')
+#Test von acDecrypt mit aufgabenstellung
+teststring = 'IVYNTWXAY'
+print('Testen von acEncrypt mit : ' + teststring)
+print('Ergebnis: ' + acDecrypt(a,b,teststring))
