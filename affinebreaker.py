7ee412a12031c9f0bc28f410ca4ecaaefc083cf5
Haisch-Nico <nico.haisch@studmail.htw-aalen.de>
2022-04-26 10:21:02 +0200

    test

--- affinebreaker.py
+++ affinebreaker.py
@@ -3,4 +3,12 @@ import ablib as ab
 import sys
 import os
 
+print("cock")
 datei=open(sys.argv[1],"r")
+print (ab.computeFrequencyTable(datei))
+ab.printFrequencyTable(datei)
+ab.computeMostFrequentChars(datei,3)
+ab.computeKeyPairs(datei)
+ab.analyzeCyphertext(datei,ab.computeKeyPairs(datei))
+datei.close()
+
