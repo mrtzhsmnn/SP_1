31769686a50a1e7d8d975d6e5e8b0b194e20ed23
Haisch-Nico <nico.haisch@studmail.htw-aalen.de>
2022-04-27 16:35:09 +0200

    Erste 3 Funktionen funktionieren

--- affinebreaker.py
+++ affinebreaker.py
@@ -4,13 +4,18 @@ import sys
 import os
 
 
+with open(sys.argv[1], "r") as datei:
+    lines = datei.read().split(',')
 
-datei=open(sys.argv[1],"r")
-print (datei.read())
-ab.computeFrequencyTable(datei)
-ab.printFrequencyTable(datei)
-ab.computeMostFrequentChars(datei,3)
-ab.computeKeyPairs(datei)
+liste = []    
+for line in lines:
+    print(line)
+    liste.append(line)
+    
+freq_table=ab.computeFrequencyTable(liste)
+ab.printFrequencyTable(freq_table)
+ab.computeMostFrequentChars(freq_table,n=6)
+ab.computeKeyPairs(liste)
 ab.analyzeCyphertext(datei,ab.computeKeyPairs(datei))
 
 datei.close()
