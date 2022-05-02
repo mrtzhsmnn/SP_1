f1d8fda9e2f5e2b226202bbfcc639b42509b9258
Haisch-Nico <nico.haisch@studmail.htw-aalen.de>
2022-05-02 14:12:50 +0200

    try except scheiße gefixt

--- Code/affinebreaker.py
+++ Code/affinebreaker.py
@@ -3,22 +3,20 @@ import ablib as ab
 import aclib as ac
 import sys
 
+
 try: 
     datei=open(sys.argv[1],"r+") #Datei öffnen und in Variable speichern
-    if(datei.read()==""): #Prüfung ob Datei leer ist
+    cipher_text=datei.read() #Inhalt der Datei in Variable speichern
+    if(cipher_text==""): #Prüfung ob Datei leer ist
         print("Datei ist leer!") #Fehlermeldung ausgeben
         sys.exit() #Programm beenden
-    datei.close() #Datei schließen
 except IndexError: #Error handling
     print("Keine Datei angegeben!")  #Fehlermeldung, wenn zu wenige Parameter angegeben wurden.
     sys.exit() #Programm beenden
 except FileNotFoundError: #Error handling 
     print("Datei existiert nicht!") #Fehlermeldung, wenn Datei nicht existiert
     sys.exit() #Programm beenden
-
-datei=open(sys.argv[1],"r+") #Datei öffnen und in Variable speichern
-cipher_text=datei.read() #Inhalt der Datei in Variable speichern
-
+datei.close() #Datei schließen
 
 decoded_content=ac.decode(cipher_text) #Inhalt der Datei mit decode-Funktion konvertieren um anschließend computeFrequencyTable-Funktion mit Zahlen aufrufen zu können
 freq_table=ab.computeFrequencyTable(decoded_content) #Frequenztabelle erstellen mit computeFrequencyTable-Funktion aus ablib 
@@ -30,4 +28,4 @@ key_pairs=ab.computeKeyPairs(a) #Schlüsselpaare erstellen
 print("Keypairs:" ,key_pairs) #Schlüsselpaare ausgeben
 
 ab.analyzeCipherText(cipher_text,key_pairs) #Analyse des verschlüsselten Textes mit allen möglichen Schlüsseln
-datei.close() #Datei schließen
+
--- Code/affinecipher.py
+++ Code/affinecipher.py
@@ -9,10 +9,11 @@ try:
     key=sys.argv[2]  #Variablen zuweisen (Schlüssel)
     path=sys.argv[3] #Variablen zuweisen (Dateipfad)
     datei=open(path,"r+") #Datei öffnen und in Variable speichern
+    oldContent=datei.read() #Inhalt der Datei in Variable speichern
     if(mode!='e' and mode!='d'): #Prüfung ob mode gültig ist   
         print("Kein gültiger Modus!") #Fehlermeldung ausgeben
         sys.exit() #Programm beenden
-    if(datei.read()==""):
+    if(oldContent==""):
         print("Datei ist leer!") #Wenn Datei leer ist Hinweis ausgeben und anschließend Programm beenden
         sys.exit()
 except IndexError: #Error handling: Wenn zu wenige Parameter angegeben wurden
@@ -21,22 +22,19 @@ except IndexError: #Error handling: Wenn zu wenige Parameter angegeben wurden
 except FileNotFoundError: #Error handling: Wenn Datei nicht existiert
     print("Datei existiert nicht!") #Fehlermeldung, wenn Datei nicht existiert
     sys.exit() #Programm beenden
-datei.close() #Datei schließen
-
-datei=open(path,"r+") #Datei öffnen und in Variable speichern
 
 a,b=ac.keyHelp(key) #Key wird in Keyhelp-Funktion zerlegt und in a und b gespeichert
 
 #Entscheiden welcher Modus gewählt wurde: 
 if(mode == 'e'): #Wenn mode "e" ist (verschlüsseln)
-    newContent=ac.acEncrypt(a,b,datei.read()) #Neuer Inhalt der Datei mit acEncrypt verschlüsselt, hierbei wird auch das Schlüsselpaar überprüft
+    newContent=ac.acEncrypt(a,b,oldContent) #Neuer Inhalt der Datei mit acEncrypt verschlüsselt, hierbei wird auch das Schlüsselpaar überprüft
     datei.close() #Datei schließen um sie anschließend löschen zu können
     os.remove(path) #Datei löschen um sie neu zu erstellen, um whitespaces zu verhindern
     datei=open(path, "w+") #Datei wird neu erstellt und geöffnet, um diese befüllen zu können
     datei.write(newContent) #verschlüsselter Inhalt wird in Datei geschrieben
     print("Datei wurde verschlüsselt!") #Meldung ausgeben
 elif(mode == 'd'): #Wenn mode "d" ist (entschlüsseln)
-    newContent=ac.acDecrypt(a,b,datei.read()) #Neuer Inhalt der Datei mit acDecrypt entschüsselt, und Schlüsselpaare überprüft
+    newContent=ac.acDecrypt(a,b,oldContent) #Neuer Inhalt der Datei mit acDecrypt entschüsselt, und Schlüsselpaare überprüft
     datei.close() #Datei schließen um sie anschließend löschen zu können
     os.remove(path) #Datei löschen um beim befüllen whitespaces zu verhindern   
     datei=open(path, "w+") #Datei wird neu erstellt und geöffnet, um diese befüllen zu können
