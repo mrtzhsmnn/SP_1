436756bf8c75e52ef57678eb052c4792ef94fc4b
Moritz Hausmann <103260415+mrtzhsmnn@users.noreply.github.com>
2022-05-11 16:04:43 +0200

    refactor: Removed spaces

--- Code/aufg6atest.py
+++ Code/aufg6atest.py
@@ -1,5 +1,5 @@
 import aclib as ac
 teststring = 'strenggeheim'
 print('Testen von acEncrypt mit: ' + teststring )
-a,b= ac.keyHelp ('db')
+a,b=ac.keyHelp ('db')
 print('Ergebnis: ' + ac.acEncrypt (a,b, teststring ))
